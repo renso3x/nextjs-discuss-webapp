f2fde8a30a43195e636ae51f6509f2a20848ac2d
Romeo Enso <romeo.enso93@gmail.com>
2024-07-13 11:21:08 +1000

    auth button sessions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,7 @@
-import { Button } from "@nextui-org/react";
-
-import * as actions from '@/actions'
-import { auth } from "@/auth";
-import Profile from "@/component/profile";
-
-export default async function Home() {
-  const session = await auth()
+export default function Home() {
   return (
     <>
-      <form action={actions.signIn}>
-        <Button type="submit">Sign In</Button>
-      </form>
-      <form action={actions.signOut}>
-        <Button type="submit">Sign Out</Button>
-      </form>
-
-      {
-        session?.user ? <div>{JSON.stringify(session.user)}</div> : <div>Signed Out</div>
-      }
-
-      <Profile />
+      Home Page
     </>
-
   );
 }
--- src/component/header.tsx
+++ src/component/header.tsx
@@ -6,10 +6,14 @@ import {
   NavbarItem,
   Input,
   Button,
-  Avatar
+  Avatar,
+  Popover,
+  PopoverTrigger,
+  PopoverContent
 } from '@nextui-org/react'
 import { auth } from "@/auth";
 import React from "react";
+import * as actions from '@/actions'
 
 export default async function Header() {
   const session = await auth()
@@ -18,16 +22,31 @@ export default async function Header() {
 
   if (session?.user) {
     authContent = (
-      <Avatar src={session.user.image || ''} />
+      <Popover placement="left">
+        <PopoverTrigger>
+          <Avatar src={session.user.image || ''} />
+        </PopoverTrigger>
+        <PopoverContent>
+          <div className="p-4">
+            <form action={actions.signOut}>
+              <Button type="submit">Sign Out</Button>
+            </form>
+          </div>
+        </PopoverContent>
+      </Popover>
     )
   } else {
     authContent = (
       <>
         <NavbarItem>
-          <Button type="submit" color="secondary" variant="bordered">SignIn</Button>
+          <form action={actions.signIn}>
+            <Button type="submit" color="secondary" variant="bordered">SignIn</Button>
+          </form>
         </NavbarItem>
         <NavbarItem>
-          <Button type="submit" color="primary" variant="flat">Sign Up</Button>
+          <form action={actions.signIn}>
+            <Button type="submit" color="primary" variant="flat">Sign Up</Button>
+          </form>
         </NavbarItem>
       </>
     )
