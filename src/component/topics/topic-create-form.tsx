93fdc5e2be1abf01711cf0a2cda07f6b9d74fc95
Romeo Enso <romeo.enso93@gmail.com>
2024-07-13 12:06:08 +1000

    wire up form validation errors in the form

--- src/component/topics/topic-create-form.tsx
+++ src/component/topics/topic-create-form.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import {
   Button,
   Input,
@@ -10,7 +12,9 @@ import * as actions from '@/actions'
 import { useFormState } from 'react-dom';
 
 export default function TopicCreateForm() {
-  const [formState, action] = useFormState(actions.createTopic, 5)
+  const [formState, action] = useFormState(actions.createTopic, {
+    errors: {}
+  })
 
   return (
     <Popover placement='left'>
@@ -23,8 +27,12 @@ export default function TopicCreateForm() {
         <form action={action}>
           <div className="flex flex-col gap-4 p-4 w-80">
             <h3 className="text-lg">Create a Topic</h3>
-            <Input label="Name" labelPlacement='outside' placeholder='Name' name="name" />
-            <Textarea label="Description" labelPlacement='outside' placeholder='Describe your topic' name="description" />
+            <Input label="Name" labelPlacement='outside' placeholder='Name' name="name"
+              isInvalid={!!formState.errors.name}
+              errorMessage={formState.errors.name?.join(', ')} />
+            <Textarea label="Description" labelPlacement='outside' placeholder='Describe your topic' name="description"
+              isInvalid={!!formState.errors.description}
+              errorMessage={formState.errors.description?.join(', ')} />
             <Button type="submit">Submit</Button>
           </div>
         </form>
