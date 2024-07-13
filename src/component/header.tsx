9df8028b7493308bbf39ebebc5ea21fe8e4904ee
Romeo Enso <romeo.enso93@gmail.com>
2024-07-13 11:27:21 +1000

    useSession in header auth to use static page

--- src/component/header.tsx
+++ src/component/header.tsx
@@ -1,57 +1,14 @@
-import Link from "next/link";
 import {
+  Input,
   Navbar,
   NavbarBrand,
   NavbarContent,
-  NavbarItem,
-  Input,
-  Button,
-  Avatar,
-  Popover,
-  PopoverTrigger,
-  PopoverContent
-} from '@nextui-org/react'
-import { auth } from "@/auth";
-import React from "react";
-import * as actions from '@/actions'
+  NavbarItem
+} from '@nextui-org/react';
+import Link from "next/link";
+import HeaderAuth from "./header-auth";
 
 export default async function Header() {
-  const session = await auth()
-
-  let authContent: React.ReactNode;
-
-  if (session?.user) {
-    authContent = (
-      <Popover placement="left">
-        <PopoverTrigger>
-          <Avatar src={session.user.image || ''} />
-        </PopoverTrigger>
-        <PopoverContent>
-          <div className="p-4">
-            <form action={actions.signOut}>
-              <Button type="submit">Sign Out</Button>
-            </form>
-          </div>
-        </PopoverContent>
-      </Popover>
-    )
-  } else {
-    authContent = (
-      <>
-        <NavbarItem>
-          <form action={actions.signIn}>
-            <Button type="submit" color="secondary" variant="bordered">SignIn</Button>
-          </form>
-        </NavbarItem>
-        <NavbarItem>
-          <form action={actions.signIn}>
-            <Button type="submit" color="primary" variant="flat">Sign Up</Button>
-          </form>
-        </NavbarItem>
-      </>
-    )
-
-  }
 
   return (
     <Navbar className="shadow mb-6">
@@ -64,7 +21,7 @@ export default async function Header() {
         </NavbarItem>
       </NavbarContent>
       <NavbarContent justify="end">
-        {authContent}
+        <HeaderAuth />
       </NavbarContent>
     </Navbar>
   )
